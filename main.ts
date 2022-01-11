7d2105b69c4f1f7effb6fed927c65210a504bace
bilbobessac <74983423+bilbobessac@users.noreply.github.com>
2022-01-11 19:10:31 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.temperature())
+    basic.pause(500)
+})
+input.onButtonPressed(Button.B, function () {
+    if (input.temperature() < 10) {
+        basic.showString("couvrez-vous")
+    }
+    if (input.temperature() >= 10 && input.temperature() <= 25) {
+        basic.showString("Bonne journée")
+    }
+    if (input.temperature() > 25) {
+        basic.showString("hydratez-vous")
+    }
+})
